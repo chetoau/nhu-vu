e0c9adb32b1b58cd6f28e260d660e9a724ab8ee3
Nhu Vu <geniuslab00@gmail.com>
2023-06-21 02:11:36 -0700

    added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./global.css";
 import Homepage from "./components/Homepage";
 import Header from './components/Header';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contacts from './components/Contacts'
+import { useState, useEffect } from 'react';
 
 function App() {
+
   return (
-    <>
-    <Header/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
