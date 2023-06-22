b7da9466e3c6b8a550335ed0f5270a123ff77f2c
Nhu Vu <geniuslab00@gmail.com>
2023-06-22 15:14:11 -0700

    changed some stuff in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,6 @@ export default function Header() {
   const activeLink = ({ isActive }) => isActive ? `${styles.activeLink}` : '';
 
 
-
-
   return (
     <header className={styles.header} id="header">
       <div className={styles.left}>
@@ -18,7 +16,7 @@ export default function Header() {
       <NavLink to="/" className={styles.logo}><img src={logo} alt="logo"/></NavLink>
       </div>
       <div className={styles.right}>
-        <div className={active ? `${styles.activeLinks}` : `${styles.links}`}>
+        <div className={styles.links}>
           <NavLink to="/projects" className={activeLink}>Projects</NavLink>
           <NavLink to="/experience" className={activeLink}>Experience</NavLink>
           <NavLink to="contacts" className={activeLink}>Contact Me</NavLink>
