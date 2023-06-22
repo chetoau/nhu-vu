28e10170d9c29babcfd20f1700e850872029de0f
Nhu Vu <geniuslab00@gmail.com>
2023-06-22 15:11:06 -0700

    fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ import logo from '../images/logo.png';
 export default function Header() {
 
   const activeLink = ({ isActive }) => isActive ? `${styles.activeLink}` : '';
-  const [active, setActive] = useState(false);
 
 
 
